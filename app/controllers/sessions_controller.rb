0fcda57ee4d3766dafa70b603a694ec61e95cfe8
Jul Jen <juliajen.prog@gmail.com>
2018-10-22 22:55:56 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
+  # whenever users do not have an access token stored in their session, they will be redirected to the Foursquare authorization URL.
 
 end
